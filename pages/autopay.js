9332a30d03bbc2d8e6aa73ac92764ee21b6764e2
baronrocketmail <baronrocketmail@yahoo.com>
2022-10-30 22:48:45 -0700

    es

--- pages/autopay.js
+++ pages/autopay.js
@@ -37,6 +37,7 @@ export default function Autopay(props){
 
     useEffect(() => {
 
+
         if (isSignInWithEmailLink(auth, window.location.href)) {
             // Additional state parameters can also be passed via URL.
             // This can be used to continue the user's intended action before triggering
@@ -44,11 +45,15 @@ export default function Autopay(props){
             // Get the email if available. This should be available if the user completes
             // the flow on the same device where they started it.
             let email = window.localStorage.getItem('emailForSignIn');
-            if (!email) {
-                // User opened the link on a different device. To prevent session fixation
-                // attacks, ask the user to provide the associated email again. For example:
-                email = window.prompt('Please provide your email for confirmation');
-            }
+
+            console.log("email: ")
+
+            console.log(window.localStorage.getItem('emailForSignIn'))
+//            if () {
+//                // User opened the link on a different device. To prevent session fixation
+//                // attacks, ask the user to provide the associated email again. For example:
+//                email = window.prompt('Please provide your email for confirmation');
+//            }
             // The client SDK will parse the code from the link for you.
             signInWithEmailLink(auth, email, window.location.href)
             .then((result) => {
@@ -65,7 +70,7 @@ export default function Autopay(props){
                 // Common errors could be invalid email and invalid or expired OTPs.
             });
         }
-    })
+    }, [])
     onAuthStateChanged(auth, (user) => {
         console.log(user)
         setUser(user)
