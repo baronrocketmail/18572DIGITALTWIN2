bf04afc9b28931cd7e0cb94f2d19746d705c9a2b
baronrocketmail <baronrocketmail@yahoo.com>
2022-11-01 09:06:21 -0700

    s

--- pages/autopay.js
+++ pages/autopay.js
@@ -97,7 +97,7 @@ function announce(z){
     const actionCodeSettings = {
         // URL you want to redirect back to. The domain (www.example.com) for this
         // URL must be in the authorized domains list in the Firebase Console.
-        url: 'https://18572cullcanyon.com/',
+        url: 'https://18572cullcanyon.com/autopay',
         // This must be true.
         handleCodeInApp: true,
     };
